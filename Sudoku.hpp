bd7f131628f8d68c5153bc84acd1fb1f79f2d5b3
RuiRabbit <rabbit922w@gmail.com>
2023-12-12 09:05:54 +0800

    turn check valid to global

--- Sudoku.hpp
+++ Sudoku.hpp
@@ -4,17 +4,73 @@
 #include <cmath>
 #include <stack>
 #include <iostream>
+// bool is_valid( const int *puzzle)
+// {
+//     int row[ N][ N] = { 0}, col[ N][ N] = { 0}, block[ N][ N] = { 0};
+
+//     for ( int i = 0; i < N; i += 1)
+//     {
+//         for ( int j = 0; j < N; j += 1)
+//         {
+//             if ( puzzle[ i * N + j] != 0)
+//             {
+//                 row[ i][ puzzle[ i * N + j] - 1] += 1;
+//                 col[ j][ puzzle[ i * N + j] - 1] += 1;
+//                 block[ (i / SUB_N) * SUB_N + j / SUB_N][ puzzle[ i * N + j] - 1] += 1;
+//             }// if
+//         }// for j
+//     }// for i
+
+//     // check duplicate
+//     for ( int i = 0; i < N; i += 1)
+//     {
+//         for ( int j = 0; j < N; j += 1)
+//         {
+//             if ( row[ i][ j] > 1 || col[ i][ j] > 1 || block[ i][ j] > 1)
+//             {
+//                 return false;
+//             }// if
+//         }// for j
+//     }// for i
+
+//     return true;
+// }
+
 
 template <int T>
 class Sudoku{
 public:
     Sudoku(std::string str);
     // point 0 based
-    static bool checkValid(const Sudoku& target) const;
+    static bool checkValid(const Sudoku& target){
+        int row[T][T] = {0}, col[T][T] = {0}, block[T][T] = {0};
+        auto &block_size = target.block_size;
+        auto &s = target.s;
+        for(int i = 0; i < T; ++i){
+            auto row_idx = i * T;
+            auto block_idx = i / block_size * block_size;
+            for(int j = 0; j < T; ++j){
+                if(target.s[row_idx + j] != '.'){
+                    int val = s[row_idx + j] - '1';
+                    row[i][val] += 1;
+                    col[j][val] += 1;
+                    block[block_idx + j / block_size][val] += 1;
+                }
+            }
+        }
+        for(int i = 0; i < T; ++i){
+            for(int j = 0; j < T; ++j){
+                if(row[i][j] > 1 || col[i][j] > 1 || block[i][j] > 1){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
     // first empty cell
     int firstEmpty();
     // -1 = empty, return true = modify successfully
-    bool modify(int idx, int val);
+    // bool modify(int idx, int val);
     std::string asString();
     std::string asSudoku();
     bool solve();
@@ -22,10 +78,11 @@ public:
         return s == rhs.s;
     }
 private:
-    std::bitset<T*T> row;
-    std::bitset<T*T> col;
-    std::bitset<T*T> block;
+    // std::bitset<T*T> row;
+    // std::bitset<T*T> col;
+    // std::bitset<T*T> block;
     std::string s = ""; // sudoku puzzle
+    int block_size;
 };
 
 using namespace std;
@@ -33,30 +90,31 @@ using namespace std;
 template<int T>
 Sudoku<T>::Sudoku(string str){
     s = str;
-    row.reset();
-    col.reset();
-    block.reset();
-    for(int idx = 0; idx < s.size(); ++idx){
-        if(s[idx] != '.'){
-            int val = s[idx] - '1';
-            int r = idx / T;
-            int c = idx % T;
-            // cout<<idx<<" "<<r<<" "<<c<<" "<<val<<" "<<r * T + val<<" "<<c * T + val<<endl;
-            row.set(r * T + val);
-            col.set(c * T + val);
-            int block_size = sqrt(T);
-            r = idx / T / block_size;
-            c = idx % T / block_size;
-            // cout<<idx<<" "<<r<<" "<<c<<" "<<val<<" "<<r * block_size * T + c * T + val<<endl;
-            block.set(r * block_size * T + c * T + val);
-        }
-    }
+    block_size = sqrt(T);
+    // row.reset();
+    // col.reset();
+    // block.reset();
+    // for(int idx = 0; idx < s.size(); ++idx){
+    //     if(s[idx] != '.'){
+    //         int val = s[idx] - '1';
+    //         // int r = idx / T;
+    //         // int c = idx % T;
+    //         // cout<<idx<<" "<<r<<" "<<c<<" "<<val<<" "<<r * T + val<<" "<<c * T + val<<endl;
+    //         // row.set(r * T + val);
+    //         // col.set(c * T + val);
+    //         // int block_size = sqrt(T);
+    //         // r = idx / T / block_size;
+    //         // c = idx % T / block_size;
+    //         // cout<<idx<<" "<<r<<" "<<c<<" "<<val<<" "<<r * block_size * T + c * T + val<<endl;
+    //         // block.set(r * block_size * T + c * T + val);
+    //     }
+    // }
 }
 // // full puzzle
-template<int T>
-bool Sudoku<T>::checkValid(const Sudoku& target) const {
+// template<int T>
+// bool Sudoku<T>::checkValid(const Sudoku& target) const {
 
-}
+// }
 // point 0 based
 // template<int T>
 // bool Sudoku<T>::checkValid(int idx, int val){
@@ -89,37 +147,43 @@ int Sudoku<T>::firstEmpty(){
     return s.size();
 }
 
-template<int T>
-bool Sudoku<T>::modify(int idx, int val){
-    char origin_val = s[idx];
-    if(s[idx] != '.'){
-        int val = s[idx] - '1';
-        int r = idx / T;
-        int c = idx % T;
-        row.reset(r * T + val);
-        col.reset(c * T + val);
-        int block_size = sqrt(T);
-        r = idx / block_size;
-        c = idx % T / block_size;
-        block.reset(r * block_size * T + c * T + val);
-    }
-    if(!checkValid(idx, val)){
-        s[idx] = origin_val;
-        return false;
-    }
-    if(val != '.'){
-        int r = idx / T;
-        int c = idx % T;
-        row.set(r * T + val);
-        col.set(c * T + val);
-        int block_size = sqrt(T);
-        r = idx / T / block_size;
-        c = idx % T / block_size;
-        block.set(r * block_size * T + c * T + val);
-        s[idx] = '1' + val;
-    }
-    return true;
-}
+// template<int T>
+// bool Sudoku<T>::modify(int idx, int val){
+//     if(val == -1){
+//         s[idx] = '.';
+//     }
+//     else{
+//         s[idx] = '1' + val;
+//     }
+//     // char origin_val = s[idx];
+//     // if(s[idx] != '.'){
+//     //     int val = s[idx] - '1';
+//     //     int r = idx / T;
+//     //     int c = idx % T;
+//     //     row.reset(r * T + val);
+//     //     col.reset(c * T + val);
+//     //     int block_size = sqrt(T);
+//     //     r = idx / block_size;
+//     //     c = idx % T / block_size;
+//     //     block.reset(r * block_size * T + c * T + val);
+//     // }
+//     // if(!checkValid(idx, val)){
+//     //     s[idx] = origin_val;
+//     //     return false;
+//     // }
+//     // if(val != '.'){
+//     //     int r = idx / T;
+//     //     int c = idx % T;
+//     //     row.set(r * T + val);
+//     //     col.set(c * T + val);
+//     //     int block_size = sqrt(T);
+//     //     r = idx / T / block_size;
+//     //     c = idx % T / block_size;
+//     //     block.set(r * block_size * T + c * T + val);
+//     //     s[idx] = '1' + val;
+//     // }
+//     return true;
+// }
 template<int T>
 string Sudoku<T>::asString(){
     return s;
@@ -128,7 +192,7 @@ template<int T>
 string Sudoku<T>::asSudoku(){
     int cntx = 0, cnty = 0;
     string ret = "";
-    int block_size = sqrt(T);
+    // int block_size = sqrt(T);
     for(int i = 0; i < T; ++i){
         if(i && i % block_size == 0){
             for(int k = 0; k < T * 2 - 1 + 2 * block_size - 2; ++k){
@@ -173,15 +237,18 @@ bool Sudoku<T>::solve(){
             return true;
         }
         vector<Sudoku> next;
+        auto origin_val = now.s[idx];
         for(int i = 0; i < T; ++i){
-            if(now.checkValid(idx, i)){
-                Sudoku tmp = now;
-                tmp.modify(idx, i);
+            now.s[idx] = '1' + i;
+            if(Sudoku<T>::checkValid(now)){
+                // Sudoku tmp = now;
+                // tmp.modify(idx, i);
                 // cout<<idx<<" "<<i<<endl;
                 // cout<<"tmp: "<<tmp.s<<endl;
-                next.push_back(tmp);
+                next.push_back(now);
             }
         }
+        now.s[idx] = origin_val;
         // lock
         for(auto &s : next){
             st.push(s);
